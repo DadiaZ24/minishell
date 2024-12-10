bb8c5b02e27a751bab9f51fd8048c0c4c1fa2146
ptorrao-git <pjsoares270@gmail.com>
2024-12-10 15:38:10 +0000

    fix: expander is complete

--- src/main.c
+++ src/main.c
@@ -32,7 +32,6 @@ int	minishell_loop(t_executor *exec, t_token **tokens)
 	create_ast(tokens, exec->ast);
 	free_token(tokens);
 	executor(exec);
-	//dup2(exec->fd_out, STDOUT_FILENO);
 	wait_pid(exec);
 	free_all(exec);
 	return (1);
