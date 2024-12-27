91f8408cff9efa94f190664efd6e4d21ffd9fcf2
DadiaZ <diogo-uds@hotmail.com>
2024-12-27 18:02:09 +0000

    fix: fixing errors

--- src/executor/builtins/exit.c
+++ src/executor/builtins/exit.c
@@ -15,12 +15,23 @@ void	exit_builtin(int status, t_executor *exec, char **mtr)
 	{
 		if (mtr[2])
 		{
-			exec->shell->status = 1;
+			set_exit_status(exec->shell, 1);
+			w_error("exit\nminishell: exit: too many arguments\n");
 			return ;
 		}
+		while(*mtr[1])
+		{
+			if (!ft_isdigit(*mtr[1]))
+			{
+				set_exit_status(exec->shell, 255);
+				w_error("exit\nminishell: exit: ");
+				w_error(mtr[1]);
+				w_error(": numeric argument required\n");
+				return ;
+			}
+			mtr[1]++;
+		}
 		status = ft_atoi(mtr[1]);
-		if (!ft_isdigit(mtr[1][0]) && mtr[1][0] != '-' && mtr[1][0] != '+')
-			status = 2;
 	}
 	free_process(exec);
 	exit(status);
